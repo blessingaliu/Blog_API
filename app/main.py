16ee8aac3d468c5b7c6b777a6122c611df881d50
blessingaliu <blessingaliu@ymail.com>
2022-06-03 16:48:48 +0100

    updated files

--- app/main.py
+++ app/main.py
@@ -1,81 +1,80 @@
-from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy.orm import Session
-
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from typing import List, Optional
+import datetime
 
 
-app = FastAPI()
+app = FastAPI(title="Blog API")
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
+blogs = []
+
+
+#pydantic Base Model
+class BlogList(BaseModel):
+    id          : int
+    title       : str
+    content     : str
+    created_at  : str = datetime.datetime.now()
+    status      : Optional[bool] = False
+
+
+#root endpoint
+@app.get('/', status_code = status.HTTP_200_OK)
+async def root():
+    return {"Homepage": "Blog Home"}
+
+
+'''
+    Get All Blog posts from blogs List
+'''
+@app.get('/get_blogs', status_code = status.HTTP_200_OK)
+async def get_blogs():
+    return {'Blog posts': blogs}
+
+
+'''
+    Create a new blog post
+'''
+@app.post('/new_blog', status_code = status.HTTP_201_CREATED)
+async def add_blog(blog: BlogList):
+    blogs.append(blog)
+    return {'message': "Blog was added.", 'Blog posts': blog}
+
+
+'''
+    Get a specific blog post using blog_id
+'''
+@app.get('/blog/{blog_id}/', status_code=status.HTTP_200_OK)
+async def get_blog(blog_id: int):
     try:
-        yield db
-    finally:
-        db.close()
+        blog = blogs[blog_id-1]
+        return blog
+    except:
+        return {'error': "Invalid Blog ID"}
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+'''
+    Update Blog Post
+'''
+@app.put('/update_blog/{blog_id}/', status_code = status.HTTP_201_CREATED)
+async def update_blog(blog_id: int, blog: BlogList):
+    try:
+        blogs[blog_id] = blog
+        return {"message": "Blog ID was updated!", "Blog posts": blog}
+
+    except:
+        return {'error': "Invalid Blog ID"}
 
 
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+'''
+    Delete Blog Post
+'''
+@app.delete('/delete/{blog_id}/', status_code = status.HTTP_200_OK)
+async def delete_blog(blog_id: int):
+    try:
+        blogs.pop(blog_id - 1)
+        return {"message": "blog was deleted."}
 
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.get("/articles/", response_model=List[schemas.Article])
-def read_articles(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    articles = crud.get_articles(db, skip=skip, limit=limit)
-    return articles
-
-
-@app.get("/articles/{article_id}", response_model=schemas.Article)
-def read_article(article_id: int, db: Session = Depends(get_db)):
-    return crud.get_object_or_404(db, models.Article, object_id=article_id)
-
-
-@app.post(
-    "/users/{user_id}/articles/",
-    response_model=schemas.Article,
-    status_code=status.HTTP_201_CREATED,
-)
-def create_user_article(
-    user_id: int, article: schemas.ArticleCreate, db: Session = Depends(get_db)
-):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User does not exist")
-    return crud.create_article(db=db, article=article, user_id=user_id)
-
-
-@app.delete("/articles/{article_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_article(article_id: int, db: Session = Depends(get_db)):
-    article = crud.get_object_or_404(db, models.Article, article_id)
-    return crud.delete_article(db=db,article=article)
-
-
-@app.patch("/articles/{article_id}", response_model=schemas.ArticleUpdate)
-def update_article(
-    article_id: int,
-    updated_fields: schemas.ArticleUpdate,
-    db: Session = Depends(get_db),
-):
-    return crud.update_article(db, article_id, updated_fields)
+    except:
+        return {"error": "Invalid BlogID."}
